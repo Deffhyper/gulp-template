934d72ec344faa9974fb7c75a5cc41fce5b5a6b8
Deff <olegkotsubko@gmail.com>
2018-01-06 17:38:36 +0200

    set base

---
+++ app/01.main-page.php
@@ -0,0 +1,19 @@
+<?php include_once "parts/head.php" ?>
+<body>
+<?php include_once "parts/header.php" ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include_once "parts/footer.php" ?>
+</body>
+</html>
---
+++ app/parts/footer.php
@@ -0,0 +1,24 @@
+<footer class="site-footer">
+
+</footer>
+
+<!--///////////////////////////////////////////////// modals //////////////////////////////////////////////-->
+<!-- Modal callback-1-->
+<div id="modal-callback-1" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <a class="close" data-dismiss="modal"></a>
+                <h4 class="modal-title"></h4>
+            </div>
+            <div class="modal-body">
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="js/libs.min.js"></script>
+<script src="js/common.js"></script>
+
